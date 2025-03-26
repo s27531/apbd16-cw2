c6e46d20784944b86439746a3269f67519b982e5
s27531 <s27531@pjwstk.edu.pl>
2025-03-26 15:00:37 +0100

    feat: Create C# project.

---
+++ apbd16-cw2/Container.cs
@@ -0,0 +1,11 @@
+﻿class Container
+{
+    public double LoadMass { get; }
+    public double Height { get; }
+    public double Weight { get; }
+    public double Depth { get; }
+    public string SerialNumber { get; }
+
+
+}
+
