cf0685f731cfcd1979b48786eb247c716317e458
s27531 <s27531@pjwstk.edu.pl>
2025-03-26 15:12:20 +0100

    feat: Add Container.cs

--- apbd16-cw2/Container.cs
+++ apbd16-cw2/Container.cs
@@ -1,11 +1,26 @@
-﻿class Container
+﻿using System.Diagnostics.Metrics;
+
+class Container(double height, double weight, double depth, double maxLoad, string containerType)
 {
-    public double LoadMass { get; }
-    public double Height { get; }
-    public double Weight { get; }
-    public double Depth { get; }
-    public string SerialNumber { get; }
+    private static int Counter { get; set; } = 0;
+    public double LoadMass { get; set; } = 0;
+    public double Height { get; } = height;
+    public double Weight { get; } = weight;
+    public double Depth { get; } = depth;
+    public string SerialNumber { get; } = $"KON-{containerType}-{Counter++}";
+    public double MaxLoad { get; } = maxLoad;
 
+    public void Empty() 
+    {
+        LoadMass = 0;
+    }
 
+    public void Load(double mass)
+    { 
+        if (LoadMass + mass > MaxLoad)
+        {
+            throw new Exception(); // TODO: Swap with OverfillException
+        }
+        LoadMass += mass;
+    }
 }
-
