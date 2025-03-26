dbb4936e3553f70e3e42c806c623102120fb0ec5
s27531 <s27531@pjwstk.edu.pl>
2025-03-26 16:47:25 +0100

    feat: Add ColdContainer.cs

---
+++ apbd16-cw2/ColdContainer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ColdContainer : Container
+{
+    private static Dictionary<string, double> products = new Dictionary<string, double>
+    {
+        { "bananas", 13.3 },
+        { "chocolate", 18 },
+        { "fish", 2 },
+        { "meat", -15 },
+        { "ice cream", -18 },
+        { "frozen pizza", -30 },
+        { "cheese", -7.2 },
+        { "sausages", 5 },
+        { "butter", 20.5 },
+        { "eggs", 19 }
+    };
+
+    public string ContentType { get; }
+    public double Temperature { get; }
+
+    
+    public ColdContainer(double height, double weight, double depth, double maxLoad, string contentType, double temperature) : base(height, weight, depth, maxLoad, "C")
+    {
+        if (!products.ContainsKey(contentType))
+        {
+            throw new Exception($"Unsupported product type: {contentType}.");
+        }
+        double minTemperature = products[contentType];
+        if (temperature < minTemperature)
+        {
+            throw new Exception($"Given temperature '{temperature}' exceeds minimal temperature '{minTemperature}'.");
+        }
+
+        ContentType = contentType;
+        Temperature = temperature;
+    }
+
+}
